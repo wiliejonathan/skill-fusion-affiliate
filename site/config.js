e80816e17852f45b3f8ebf44a378b1c61a67e129
wiliejonathan <117905663+wiliejonathan@users.noreply.github.com>
2026-09-26 15:02:09 +0700

    config: connect GitHub Pages to deployed Apps Script API

--- site/config.js
+++ site/config.js
@@ -1,5 +1,5 @@
 window.SKILL_FUSION_CONFIG={
-  APPS_SCRIPT_URL:"",
+  APPS_SCRIPT_URL:"https://script.google.com/macros/s/AKfycbwEaWGv8ykfHPDqI4eUAp0U9FqYWeMb3RsS57K9QDsxnvbOI9h0HArIVASgn2llUZqaNQ/exec",
   SHEET_ID:"1V3LTciKM0AAXQAbdk-1eNk6Ie0aXL_ksvSDBzDVjUI0",
   SHEET_URL:"https://docs.google.com/spreadsheets/d/1V3LTciKM0AAXQAbdk-1eNk6Ie0aXL_ksvSDBzDVjUI0/edit",
   API_VERSION:"v1"
