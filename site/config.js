706e3e4832aea7f7181c51f69501cff7ab46e5d9
wiliejonathan <117905663+wiliejonathan@users.noreply.github.com>
2026-09-26 14:11:40 +0700

    migrate: add free Google Sheets Apps Script GitHub Pages stack

---
+++ site/config.js
@@ -0,0 +1,6 @@
+window.SKILL_FUSION_CONFIG={
+  APPS_SCRIPT_URL:"",
+  SHEET_ID:"1V3LTciKM0AAXQAbdk-1eNk6Ie0aXL_ksvSDBzDVjUI0",
+  SHEET_URL:"https://docs.google.com/spreadsheets/d/1V3LTciKM0AAXQAbdk-1eNk6Ie0aXL_ksvSDBzDVjUI0/edit",
+  API_VERSION:"v1"
+};
