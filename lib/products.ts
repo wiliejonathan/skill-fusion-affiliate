a6d3aaa88d3cac5e2d2474ad44d375f0dfcba840
wiliejonathan <117905663+wiliejonathan@users.noreply.github.com>
2026-09-26 20:08:50 +0700

    fix: ship current 5-product snapshot for first paint

--- lib/products.ts
+++ lib/products.ts
@@ -16,70 +16,122 @@ export type Product={
 export const categories=["Semua","Charging & Cable"];
 
 export const products:Product[]=[
-{
-  sequence:1,
-  id:"ACO-60021-00244-00014",
-  canonicalProductId:"ACO-60021-00244-00014",
-  name:"ACMIC Braided Line Kabel Data Charger 100cm Fast Charging Cable GC100 / GL100 / GM100",
-  brand:"ACMIC",
-  category:"Charging & Cable",
-  images:[
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-112494305/acmic_acmic_braided_line_kabel_data_charger_100cm_fast_charging_cable_-gc100-gl100-gm100-_full45_njeqi5ul.jpg",
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/93/MTA-112494305/acmic_acmic_braided_line_kabel_data_fast_charging_iphone-type_c-micro_usb_1m_full15_ph0p14k5.jpg",
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/93/MTA-112494305/acmic_acmic_braided_line_kabel_data_fast_charging_iphone-type_c-micro_usb_1m_full16_mwsi08wd.jpg",
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/93/MTA-112494305/acmic_acmic_braided_line_kabel_data_fast_charging_iphone-type_c-micro_usb_1m_full17_fecq856n.jpg",
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/93/MTA-112494305/acmic_acmic_braided_line_kabel_data_fast_charging_iphone-type_c-micro_usb_1m_full18_gr5fkrpk.jpg",
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-112494305/acmic_acmic_braided_line_kabel_data_charger_100cm_fast_charging_cable_-gc100-gl100-gm100-_full44_k26116c9.jpg"
-  ],
-  affiliateUrl:"https://s.blibli.com/GNtk/0qrtsw3f",
-  badge:"Blibli Affiliate",
-  features:["100 cm","Braided cable","Fast charging"]
-},
-{
-  sequence:2,
-  id:"ACO-60021-00070-00001",
-  canonicalProductId:"ACO-60021-00070-00001",
-  name:"ACMIC FC100 CFC100 kabel data charger usb type c 100CM fast charging cable hitam",
-  brand:"ACMIC",
-  category:"Charging & Cable",
-  images:[
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full14_ge6ro4m0.jpeg",
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full15_frg35cse.png",
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full16_t0oy1nvi.png",
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full17_c2fy0i99.jpeg",
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full18_tj420zxl.jpeg",
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full19_grw1dvf1.jpeg",
-    "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full20_vabiy15q.jpeg"
-  ],
-  affiliateUrl:"https://s.blibli.com/GNtk/nppiag7f",
-  badge:"Blibli Affiliate",
-  features:["100 cm","USB Type-C","Fast charging"]
-},
-{
-  sequence:3,
-  id:"XIO-60022-01141-00001",
-  canonicalProductId:"XIO-60022-01141-00001",
-  name:"XIAOMI cable 6A type a to type c",
-  brand:"XIAOMI",
-  category:"Charging & Cable",
-  images:[
-    "https://4phones.eu/cdn/shop/files/90000810874_A.jpg?v=1770447730",
-    "https://xlineparts.com/storage/images/products/1738406224_7799.jpg"
-  ],
-  affiliateUrl:"https://s.blibli.com/GNtk/5qzuieiw",
-  badge:"Blibli Affiliate",
-  features:["USB Type-C","6A","Fast charging"]
-},
-{
-  sequence:4,
-  id:"ACO-60021-00234-00001",
-  canonicalProductId:"ACO-60021-00234-00001",
-  name:"ACMIC PDC100 power delivery pd 100CM cable usb type c to usb type c",
-  brand:"ACMIC",
-  category:"Charging & Cable",
-  images:["https://acmic.id/cdn/shop/files/CABLE_PDC100_1000x.jpg?v=1720595179"],
-  affiliateUrl:"https://s.blibli.com/GNtk/gd0wp7zz",
-  badge:"Blibli Affiliate",
-  features:["100 cm","USB Type-C","Power Delivery"]
-}
+  {
+    "sequence": 1,
+    "id": "ACO-60021-00244-00014",
+    "canonicalProductId": "ACO-60021-00244-00014",
+    "name": "ACMIC Braided Line Kabel Data Charger 100cm Fast Charging Cable GC100 / GL100 / GM100",
+    "brand": "ACMIC",
+    "category": "Charging & Cable",
+    "images": [
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-112494305/acmic_acmic_braided_line_kabel_data_charger_100cm_fast_charging_cable_-gc100-gl100-gm100-_full45_njeqi5ul.jpg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/93/MTA-112494305/acmic_acmic_braided_line_kabel_data_fast_charging_iphone-type_c-micro_usb_1m_full15_ph0p14k5.jpg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/93/MTA-112494305/acmic_acmic_braided_line_kabel_data_fast_charging_iphone-type_c-micro_usb_1m_full16_mwsi08wd.jpg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/93/MTA-112494305/acmic_acmic_braided_line_kabel_data_fast_charging_iphone-type_c-micro_usb_1m_full17_fecq856n.jpg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/93/MTA-112494305/acmic_acmic_braided_line_kabel_data_fast_charging_iphone-type_c-micro_usb_1m_full18_gr5fkrpk.jpg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-112494305/acmic_acmic_braided_line_kabel_data_charger_100cm_fast_charging_cable_-gc100-gl100-gm100-_full44_k26116c9.jpg"
+    ],
+    "affiliateUrl": "https://s.blibli.com/GNtk/0qrtsw3f",
+    "badge": "Blibli Affiliate",
+    "features": [
+      "100 cm",
+      "Fast charging"
+    ],
+    "price": null,
+    "currency": null
+  },
+  {
+    "sequence": 2,
+    "id": "ACO-60021-00070-00001",
+    "canonicalProductId": "ACO-60021-00070-00001",
+    "name": "ACMIC FC100 CFC100 kabel data charger usb type c 100CM fast charging cable hitam",
+    "brand": "ACMIC",
+    "category": "Charging & Cable",
+    "images": [
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full14_ge6ro4m0.jpeg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full15_frg35cse.png",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full16_t0oy1nvi.png",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full17_c2fy0i99.jpeg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full18_tj420zxl.jpeg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full19_grw1dvf1.jpeg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-3937306/acmic_acmic_fc100_kabel_data_charger_usb_type_c_100cm_fast_charging_cable_-_hitam_full20_vabiy15q.jpeg"
+    ],
+    "affiliateUrl": "https://s.blibli.com/GNtk/nppiag7f",
+    "badge": "Blibli Affiliate",
+    "features": [
+      "100 cm",
+      "USB Type-C",
+      "Fast charging"
+    ],
+    "price": null,
+    "currency": null
+  },
+  {
+    "sequence": 3,
+    "id": "XIO-60022-01141-00001",
+    "canonicalProductId": "XIO-60022-01141-00001",
+    "name": "XIAOMI cable 6A type a to type c",
+    "brand": "XIAOMI",
+    "category": "Charging & Cable",
+    "images": [
+      "https://i02.appmifile.com/485_item_id/17/02/2025/1ca3975822d198c41b075f895552eedf.png",
+      "https://i02.appmifile.com/mi-com-product/fly-birds/xiaomi-6a-type-a-to-type-c-cable/de180110a3ff43719b8c159073b904e9.jpg",
+      "https://i02.appmifile.com/mi-com-product/fly-birds/xiaomi-6a-type-a-to-type-c-cable/b28c8e8292c65b13c7802f25ac2f2718.png",
+      "https://i02.appmifile.com/mi-com-product/fly-birds/xiaomi-6a-type-a-to-type-c-cable/d23ad07eaa36bb64d5cdb360bacdb816.jpg",
+      "https://i02.appmifile.com/mi-com-product/fly-birds/xiaomi-6a-type-a-to-type-c-cable/c2d18bdac7d3516d3b328ff0dc9687cb.jpg"
+    ],
+    "affiliateUrl": "https://s.blibli.com/GNtk/5qzuieiw",
+    "badge": "Blibli Affiliate",
+    "features": [
+      "USB Type-C",
+      "Fast charging"
+    ],
+    "price": null,
+    "currency": null
+  },
+  {
+    "sequence": 4,
+    "id": "ACO-60021-00234-00001",
+    "canonicalProductId": "ACO-60021-00234-00001",
+    "name": "Online Mall Blibli, Belanja Online Aman, Nyaman & Terpercaya!",
+    "brand": "ACMIC",
+    "category": "Charging & Cable",
+    "images": [
+      "https://acmic.id/cdn/shop/files/CABLE_PDC100_1000x.jpg?v=1720595179"
+    ],
+    "affiliateUrl": "https://s.blibli.com/GNtk/gd0wp7zz",
+    "badge": "Blibli Affiliate",
+    "features": [
+      "Blibli Affiliate"
+    ],
+    "price": null,
+    "currency": null
+  },
+  {
+    "sequence": 5,
+    "id": "ACO-60021-00122-00005",
+    "canonicalProductId": "ACO-60021-00122-00005",
+    "name": "ACMIC CFC100 usb type c fast charging cable kabel data charger 100 cm",
+    "brand": "ACMIC",
+    "category": "Charging & Cable",
+    "images": [
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-6370783/acmic_acmic_cfc100_usb_type_c_fast_charging_cable_kabel_data_charger_-100_cm-_full02_r5kwu2kd.jpg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-6370783/acmic_acmic_cfc100_usb_type_c_fast_charging_cable_kabel_data_charger_-100_cm-_full03_lksk36kw.jpg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-6370783/acmic_acmic_cfc100_usb_type_c_fast_charging_cable_kabel_data_charger_-100_cm-_full01_qypmsyee.jpg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-6370783/acmic_acmic_cfc100_usb_type_c_fast_charging_cable_kabel_data_charger_-100_cm-_full04_pyp47xtt.jpg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-6370783/acmic_acmic_cfc100_usb_type_c_fast_charging_cable_kabel_data_charger_-100_cm-_full05_d1dlpkjc.jpg",
+      "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-6370783/acmic_acmic_cfc100_usb_type_c_fast_charging_cable_kabel_data_charger_-100_cm-_full06_mpxrk7fj.jpg",
+      "https://acmic.id/cdn/shop/files/FLEXYLINE_GambarUtamaCFC_1080x.jpg?v=1698295560",
+      "https://acmic.id/cdn/shop/files/CABLE_FLEXYLINE_7b17bc56-50db-4378-9bd0-c004ca8d6e89_1080x.jpg?v=1698295560"
+    ],
+    "affiliateUrl": "https://s.blibli.com/GNtk/8394nrit",
+    "badge": "Blibli Affiliate",
+    "features": [
+      "100 cm",
+      "USB Type-C",
+      "Fast charging"
+    ],
+    "price": null,
+    "currency": null
+  }
 ];
